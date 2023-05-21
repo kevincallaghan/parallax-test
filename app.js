324e7c9f4c2678b49da58db89006d4539dfc23c2
kevincallaghan <kevincallaghan5@yahoo.com>
2023-05-21 00:12:51 -0500

    yup yup

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-// const parallaxElements = document.querySelectorAll(".parallax");
-
-// let xValue = 0;
-// let yValue = 0;
-// let zValue = 0;
-
-// window.addEventListener("mousemove", (e) => {
-//   const windowWidth = window.innerWidth;
-//   const windowHeight = window.innerHeight;
-  
-//   xValue = e.clientX - windowWidth / 2;
-//   yValue = e.clientY - windowHeight / 2;
-
-//   const zValue = (e.clientX / windowWidth - 0.5) * 2;
-
-//   let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
-//   let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
-
-//   parallaxElements.forEach(element => {
-//     const speedX = parseFloat(element.dataset.speedx);
-//     const speedY = parseFloat(element.dataset.speedy);
-//     const speedZ = parseFloat(element.dataset.speedz);
-
-//     const transformX = `${-xValue * speedX / windowWidth * 100}px`;
-//     const transformY = `${yValue * speedY / windowHeight * 100}px`;
-//     const transformZ = ``;
-//     element.style.transform = `translate(${transformX}, ${transformY}) translateZ(${zValue * speedZ}px)`;
-//   });
-// });
-
-
-
 const parallaxElements = document.querySelectorAll(".parallax");
 
 let xValue = 0;
@@ -42,8 +10,13 @@ function updateParallax(e) {
 
   if (timeline.isActive()) return;
 
-  xValue = e.clientX - windowWidth / 2;
-  yValue = e.clientY - windowHeight / 2;
+  if (e.type === "mousemove") {
+    xValue = e.clientX - windowWidth / 2;
+    yValue = e.clientY - windowHeight / 2;
+  } else if (e.type === "touchmove") {
+    xValue = e.touches[0].clientX - windowWidth / 2;
+    yValue = e.touches[0].clientY - windowHeight / 2;
+  }
 
   parallaxElements.forEach(element => {
     const speedX = parseFloat(element.dataset.speedx);
@@ -53,7 +26,7 @@ function updateParallax(e) {
     const transformX = `${-xValue * speedX / windowWidth * 100}px`;
     const transformY = `${yValue * speedY / windowHeight * 100}px`;
 
-    const distanceX = Math.abs(e.clientX - element.getBoundingClientRect().left - element.offsetWidth / 2);
+    const distanceX = Math.abs(xValue - element.getBoundingClientRect().left - element.offsetWidth / 2);
     const zIncrement = distanceX * speedZ;
 
     element.style.transform = `translate(${transformX}, ${transformY}) translateZ(${zIncrement}px)`;
@@ -65,6 +38,14 @@ const parallaxElementsNotBg = document.querySelectorAll(".parallax:not(.bg-img)"
 window.addEventListener("mousemove", updateParallax);
 window.addEventListener("mouseout", () => {
   // Reset the parallax effect when the mouse is outside the window
+  parallaxElementsNotBg.forEach(element => {
+    element.style.transform = "none";
+  });
+});
+
+window.addEventListener("touchmove", updateParallax);
+window.addEventListener("touchcancel", () => {
+  // Reset the parallax effect when touch is canceled
   parallaxElementsNotBg.forEach(element => {
     element.style.transform = "none";
   });
@@ -82,9 +63,3 @@ timeline.fromTo(
     stagger: 0.4,
   }
 );
-
-
-
-
-
-
